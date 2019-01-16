4ee91714f1df418eb0ab39c09af0696f80c22335
Jessica Fix <jfix@veeloinc.com>
2019-01-16 09:31:21 -0800

    Style hero section

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-import heroImg from '../../img/hero-image.jpg';
-
 export default class Hero extends React.Component {
 
   render () {
     return (
         <div className='hero-section'>
-            <img src={heroImg} alt='people stretching in exercise class' />
+            <div className='hero-text'>
+              <h1>Working out<br /> has never been so fun</h1>
+              <p>Welcome to the center of it all. The Fitness Shop presents you with hundreds of workout programs designed to guide your weight loss journey.</p>
+            </div>
         </div>
     );
   }

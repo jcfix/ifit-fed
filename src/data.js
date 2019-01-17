766bd2170703a125aa70a2583889108325ec9762
Jessica Fix <jfix@veeloinc.com>
2019-01-16 19:06:34 -0800

    Add data and assets

--- src/data.js
+++ src/data.js
@@ -10,6 +10,7 @@ export const mainLinks = [
     url: '#'
   }
 ]
+
 export const subLinks = [
   {
     key: '1',
@@ -36,3 +37,97 @@ export const subLinks = [
     isSelected: false
   }
 ]
+
+export const footerLinks = [
+  {
+    key: '1',
+    text: 'Blog + Videos',
+    url: '#'
+  },
+  {
+    key: '2',
+    text: 'Shop',
+    url: '#'
+  },
+  {
+    key: '3',
+    text: 'Company',
+    url: '#'
+  },
+  {
+    key: '4',
+    text: 'Press',
+    url: '#'
+  },
+  {
+    key: '5',
+    text: 'Jobs',
+    url: '#'
+  },
+  {
+    key: '6',
+    text: 'Support',
+    url: '#'
+  },
+  {
+    key: '7',
+    text: 'Legal',
+    url: '#'
+  },
+  {
+    key: '8',
+    text: 'Contact',
+    url: '#'
+  },
+]
+
+// export const socialLinks = [
+//   {
+//     key: '1',
+//     alt: 'Blog + Videos',
+//     url: '#',
+//     src: '/img/icons/facebook.png'
+//   },
+//   {
+//     key: '2',
+//     alt: 'Shop',
+//     url: '#',
+//     src: '../img/icons/twitter.png'
+//   },
+//   {
+//     key: '3',
+//     alt: 'Company',
+//     url: '#',
+//     src: '../img/icons/google.png'
+//   },
+//   {
+//     key: '4',
+//     alt: 'Press',
+//     url: '#',
+//     src: '/img/icons/facebook.png'
+//   },
+//   {
+//     key: '5',
+//     alt: 'Jobs',
+//     url: '#',
+//     src: '/img/icons/facebook.png'
+//   },
+//   {
+//     key: '6',
+//     alt: 'Support',
+//     url: '#',
+//     src: '/img/icons/facebook.png'
+//   },
+//   {
+//     key: '7',
+//     alt: 'Legal',
+//     url: '#',
+//     src: '/img/icons/facebook.png'
+//   },
+//   {
+//     key: '8',
+//     alt: 'Contact',
+//     url: '#',
+//     src: '/img/icons/facebook.png'
+//   },
+// ]
